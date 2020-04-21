99d62fd56cb2879c8813ce9d263df7e38cfa2b7d
bencoder123 <benjamintg@web.de>
2020-04-21 10:33:32 +0200

    Added the Card Class

---
+++ Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+A class that acts as one flashcard. It stores 
+ strings for two sides of the card. 
+
+*/
+namespace Flashcards
+{
+    class Card
+    {  
+        public string belongsTo = ""; //the path of the card group that contains this card
+        public string sideOne = ""; //first side of the card
+        public string sideTwo = ""; //second side of the card
+        public bool viewed = false; //a 'viewed' variable to determine if the card has been viewed
+
+
+        
+        /// populates a side of a card with a string 
+        public void writeToSide(int side, string content)
+        {
+            if (side == 1)
+            {
+                this.sideOne = content;
+            }
+            else
+            {
+                this.sideTwo = content;
+            }
+
+        }
+    }
+}
