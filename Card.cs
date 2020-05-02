545b83407bbb4a4bb56011daa6a3e5e7806eef1b
bencoder123 <benjamintg@web.de>
2020-05-02 17:09:08 +0200

    Add setSide() to Card class

--- Card.cs
+++ Card.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 /*
 A class that acts as one flashcard. It stores 
  strings for two sides of the card. 
@@ -12,10 +13,33 @@ namespace Flashcards
     class Card
     {
         string id = "";
+        string sideOne = "";
+        string sideTwo = "";
         bool viewed = false;
 
 
-        /// populates a side of a card with a string 
+        //populates a side of a card with a string
+        public void setSide(int side, string content)
+        {
+            if (side == 1)
+            {
+                this.sideOne = content;
+            }
+            else
+            {
+                this.sideTwo = content;
+            }
 
+        }
+
+        //set the 'viewed' variable to true 
+        //so that it cannot be read twice in the same
+        //session.
+        public void isViewed()
+        {
+            this.viewed = true;
+        }
     }
+
+
 }
