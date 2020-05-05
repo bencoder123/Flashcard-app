49a04e85944cce4b413d25f78fd057aa147543e6
bencoder123 <benjamintg@web.de>
2020-05-05 13:39:32 +0200

    Fix a bug in readCards()

--- Card.cs
+++ Card.cs
@@ -16,13 +16,6 @@ namespace Flashcards
         public bool viewed = false;
 
 
-        public Card(string id, string side1, string side2)
-        {
-            this.id = id;
-            this.side1 = side1;
-            this.side2 = side2;
-            viewed = false;
-        }
 
 
 
